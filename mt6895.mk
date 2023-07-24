490122975c8dfe1ab7269f42e9d302e19b3b36e4
bengris32 <bengris32@protonmail.ch>
2023-07-24 10:22:51 +0100

    mt6895-common: Configure vendor logtags
    Change-Id: I089030c954252cfa6707232e617eaaee9b5f4015
    Signed-off-by: alextrack2013 <alextrack2013@gmail.com>

--- mt6895.mk
+++ mt6895.mk
@@ -311,6 +311,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-3.9.1-vendorcompat:64
