90b668e25d4fe886762c6790293e4b9684ef2997
bengris32 <bengris32@protonmail.ch>
2023-11-13 16:12:47 +0000

    mt6895-common: Inherit `generic_ramdisk` product configuration
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I01fb5cd9a7e1f461848bcabd8874aed9f841cbac

--- mt6895.mk
+++ mt6895.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
