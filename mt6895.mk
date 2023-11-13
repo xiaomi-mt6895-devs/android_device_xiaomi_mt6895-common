e2c28790786fc8c5526c6beaaa3593cf5043a150
Bowgo Tsai <bowgotsai@google.com>
2023-11-13 16:18:50 +0000

    mt6895-common: Adding developer GSI keys
    Change-Id: I6f7929e04a76f71ae80c467c462634c4a2e98fc0

--- mt6895.mk
+++ mt6895.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit generic_ramdisk product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
