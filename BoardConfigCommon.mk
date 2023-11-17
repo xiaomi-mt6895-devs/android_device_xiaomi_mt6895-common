b6aabb6ef1c81ce052685e9032ae7ba69bb5ad4d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-17 23:30:27 +0000

    mt6895-common: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,6 +70,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 
 BOARD_BOOTCONFIG := androidboot.selinux=permissive
+BOARD_BOOTCONFIG += androidboot.init_fatal_reboot_target=recovery
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
