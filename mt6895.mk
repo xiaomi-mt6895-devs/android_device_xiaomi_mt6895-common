ae0c71b1107e491399a019f3627b7830c5285587
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-18 20:55:30 +0000

    mt6895-common: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8

--- mt6895.mk
+++ mt6895.mk
@@ -87,9 +87,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 # Rootdir
 PRODUCT_PACKAGES += \
