cd6830928e783a2babf1165caffe02fc251b3860
bengris32 <bengris32@protonmail.ch>
2023-11-18 21:27:44 +0000

    mt6895-common: Build common Xiaomi Light service
    Change-Id: I19abe561ab14f4f99190e5d45b8d71178d39a549
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6895.mk
+++ mt6895.mk
@@ -102,6 +102,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0.vendor
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6895 \
