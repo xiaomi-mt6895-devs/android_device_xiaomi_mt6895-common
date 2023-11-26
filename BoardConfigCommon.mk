68d19e8b60dd505924cc04bc23fe4cf7cc95a65e
Christopher Ferris <cferris@google.com>
2023-11-26 17:28:38 +0000

    mt6895-common: Switch to IGNORE_PREFER32_ON_DEVICE.
    In aosp, the name of the variable changed from
    FORCE_MULTILIB_FIRST_ON_DEVICE to IGNORE_PREFER32_ON_DEVICE.
    
    So actually modify this so the new devices don't have 32 bit
    servers.
    
    Test: Verified that drmserver/mediaserver are 64 bit processes.
    Change-Id: I282f5129bcc91d99e924f514b01fc4e7e37e9ea9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,7 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 ZYGOTE_FORCE_64 := true
 
 # Force any prefer32 targets to be compiled as 64 bit.
-FORCE_MULTILIB_FIRST_ON_DEVICE := true
+IGNORE_PREFER32_ON_DEVICE := true
 
 # Build the 32 bit targets
 TARGET_2ND_ARCH := arm
