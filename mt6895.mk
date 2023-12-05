45b67a2ae520db282ff5e55ab1e2b6bc83f3bb0f
bengris32 <bengris32@protonmail.ch>
2023-12-05 18:07:36 +0000

    mt6895-common: Configure common MediaTek SE stack
    Change-Id: I7479a41b2484851903bde188d1349864bc44e08f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6895.mk
+++ mt6895.mk
@@ -217,6 +217,10 @@ PRODUCT_PACKAGES += \
     init.insmod.sh \
     init.insmod.mt6895.cfg
 
+# Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.2.vendor:64
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
