d3bde22d7a91d59d909f0f326de44897220c8194
Giovanni Ricca <giovanniricca@proton.me>
2023-12-24 15:27:58 +0100

    mt6895-common: Move to sensors 2.1 multihal
    Change-Id: Ia5970d5ba681e9cb23700580f13c542021d08bc8

--- mt6895.mk
+++ mt6895.mk
@@ -336,8 +336,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@1.0-service \
-    android.hardware.sensors@1.0-impl-xiaomi \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge \
     libshim_sensors
 
