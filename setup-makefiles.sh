c3bdd4a5ff856b6147376fae9102f0ea39ffa7cd
alextrack2013 <alextrack2013@gmail.com>
2024-02-06 17:36:31 +0100

    mt6895-common: Add support for aristotle
    Change-Id: I0eab775c9cf378759fafe7f784296b457727fe56

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "plato xaga"
+write_headers "aristotle plato xaga"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
