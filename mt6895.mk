b5142f95a5636f7a0ba6e461d21bde7c07d666a1
Michael Bestas <mkbestas@lineageos.org>
2024-03-06 12:51:55 +0000

    mt6895-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- mt6895.mk
+++ mt6895.mk
@@ -229,6 +229,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor \
