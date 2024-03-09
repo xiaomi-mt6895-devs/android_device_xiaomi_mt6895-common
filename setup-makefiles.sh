8d1b595a33bc0bf900944fd534fd1b29cc6eb35a
Michael Bestas <mkbestas@lineageos.org>
2024-03-09 00:01:01 +0000

    mt6895-common: Add firmware extraction support
    Change-Id: If6cb6334a8f7b2fcc77c8947569eba1b2002190a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -49,6 +49,10 @@ if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     elif [ "${TARGET_TEE}" == "mitee" ]
     then
       write_makefiles "${MY_DIR}/../../${VENDOR_COMMON}/${DEVICE_COMMON}/proprietary-files-mitee.txt" true
+    fi
+
+    if [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
+        append_firmware_calls_to_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt"
     fi
 
     # Finish
