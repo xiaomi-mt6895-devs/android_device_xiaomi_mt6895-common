7c39b17d7ff3f8aa70c5f70aeb04b45941920b1f
bengris32 <bengris32@protonmail.ch>
2024-08-30 16:17:44 +0100

    mt6895-common: Move to OSS sensors subhal wrapper
    Change-Id: Ic2c3bb2b75bc6306f91f4a3f8f80062a5c602007
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6895.mk
+++ mt6895.mk
@@ -389,6 +389,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-subhal-impl-1.0 \
     android.hardware.sensors-service.xiaomi-multihal \
     libsensorndkbridge \
     libshim_sensors
